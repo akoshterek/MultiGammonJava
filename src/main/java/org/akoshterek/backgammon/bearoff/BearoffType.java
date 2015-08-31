14e2e67f3da5919d2f0589d8770a1a9db7453a5b
akoshterek <koshterek@gmail.com>
2015-08-31 22:52:24 +0300

    Added bearoff databases

---
+++ src/main/java/org/akoshterek/backgammon/bearoff/BearoffType.java
@@ -0,0 +1,11 @@
+package org.akoshterek.backgammon.bearoff;
+
+/**
+ * @author Alex
+ *         date 31.08.2015.
+ */
+public enum BearoffType {
+    BEAROFF_INVALID,
+    BEAROFF_ONESIDED,
+    BEAROFF_TWOSIDED
+}
