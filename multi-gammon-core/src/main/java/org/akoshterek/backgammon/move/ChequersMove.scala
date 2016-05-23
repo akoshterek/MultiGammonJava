b244f4f85f33262bce07eedf75ba57e8a880f0c0
Oleksii Koshterek <oleksii.koshterek@powerhouse.nl>
2016-05-23 11:30:19 +0200

    Scala migration

--- multi-gammon-core/src/main/java/org/akoshterek/backgammon/move/ChequersMove.scala
+++ multi-gammon-core/src/main/java/org/akoshterek/backgammon/move/ChequersMove.scala
@@ -1,55 +1,55 @@
 package org.akoshterek.backgammon.move
 
-import java.util
-
 import org.akoshterek.backgammon.board.BoardFormatter
-import java.util.Comparator
 
 class ChequersMove {
 
-  class ChequerMove {
-    var from: Int = -1
-    var to: Int = -1
-  }
+    class ChequerMove extends Ordered[ChequerMove] {
+        var from: Int = -1
+        var to: Int = -1
 
-  var move: Array[ChequerMove] = Array[ChequerMove](
-    new ChequerMove,
-    new ChequerMove,
-    new ChequerMove,
-    new ChequerMove
-  )
+        override def compare(that: ChequerMove): Int = {
+            val o1 = that
+            val o2 = this
 
-  def copyFrom(o: ChequersMove) {
-    for (i <- move.indices) {
-        move(i).from = o.move(i).from
-        move(i).to = o.move(i).to
+            if (o1.from > o2.from) {
+                1
+            }
+            else if (o1.from < o2.from) {
+                -1
+            }
+            else {
+                Integer.valueOf(o1.to).compareTo(o2.to)
+            }
+        }
     }
-  }
 
-  private def sort() {
-    util.Arrays.sort (move, new Comparator[ChequerMove] {
-      override def compare(o2: ChequerMove, o1: ChequerMove): Int = {
-        if (o1.from > o2.from) {
-           1
-        }
-        else if (o1.from < o2.from) {
-          -1
-        }
-        else {
-          Integer.valueOf(o1.to).compareTo(o2.to)
-        }
-      }
-    })
-  }
+    var move: Array[ChequerMove] = Array[ChequerMove](
+        new ChequerMove,
+        new ChequerMove,
+        new ChequerMove,
+        new ChequerMove
+    )
 
-  override def toString: String = {
-    sort()
-    val builder: StringBuilder = new StringBuilder
-    for (aMove <- move) {
-      if (aMove.from >= 0) {
-        builder.append(String.format("%s/%s ", BoardFormatter.formatPointPlain(aMove.from + 1), BoardFormatter.formatPointPlain(aMove.to + 1)))
-      }
+    def copyFrom(o: ChequersMove) {
+        for (i <- move.indices) {
+            move(i).from = o.move(i).from
+            move(i).to = o.move(i).to
+        }
     }
-    builder.toString
-  }
+
+    private def sort() {
+        scala.util.Sorting.quickSort(move)
+    }
+
+    override def toString: String = {
+        sort()
+        val builder: StringBuilder = new StringBuilder
+        for (aMove <- move) {
+            if (aMove.from >= 0) {
+                builder.append(String.format("%s/%s ", BoardFormatter.formatPointPlain(aMove.from + 1), BoardFormatter.formatPointPlain(aMove.to + 1)))
+            }
+        }
+        builder.toString
+    }
 }
