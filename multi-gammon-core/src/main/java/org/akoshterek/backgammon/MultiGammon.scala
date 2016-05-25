0cf5322633f327b77a87dff17b16cb61693bef86
Alex Koshterek <kosterek@gmail.com>
2016-05-25 17:48:51 +0200

    Scala migration

--- multi-gammon-core/src/main/java/org/akoshterek/backgammon/MultiGammon.scala
+++ multi-gammon-core/src/main/java/org/akoshterek/backgammon/MultiGammon.scala
@@ -2,11 +2,9 @@ package org.akoshterek.backgammon
 
 import org.akoshterek.backgammon.dispatch.Dispatcher
 
-object MultiGammon {
-    def main(args: Array[String]) {
-        val dispatcher: Dispatcher = new Dispatcher
-        if (dispatcher.init(args)) {
-            dispatcher.run()
-        }
+object MultiGammon extends App{
+    val dispatcher: Dispatcher = new Dispatcher
+    if (dispatcher.init(args)) {
+        dispatcher.run()
     }
 }
