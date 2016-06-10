16480450fa24a6bf124607220cb95b4b76d51745
akoshterek <koshterek@gmail.com>
2016-06-10 20:50:20 +0200

    Scala migration

--- multi-gammon-core/src/main/java/org/akoshterek/backgammon/agent/fa/AbsNeuralNetworkFA.scala
+++ multi-gammon-core/src/main/java/org/akoshterek/backgammon/agent/fa/AbsNeuralNetworkFA.scala
@@ -1,8 +1,6 @@
 package org.akoshterek.backgammon.agent.fa
 
 import org.encog.neural.networks.BasicNetwork
-import org.encog.persist.EncogDirectoryPersistence
-import java.io.File
 
 abstract class AbsNeuralNetworkFA(val network: BasicNetwork) extends NeuralNetworkFA {
 
