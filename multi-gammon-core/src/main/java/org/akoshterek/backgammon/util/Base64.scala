913f60906dd915dea0530a28fe1627ad119ffacf
Oleksii Koshterek <oleksii.koshterek@irdeto.com>
2017-04-10 14:52:48 +0000

    functional normalizer

--- multi-gammon-core/src/main/java/org/akoshterek/backgammon/util/Base64.scala
+++ multi-gammon-core/src/main/java/org/akoshterek/backgammon/util/Base64.scala
@@ -8,11 +8,11 @@ object Base64 {
     val aszBase64: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
     def base64(ch: Byte): Byte = {
-        if (ch >= 'A' && ch <= 'Z') return (ch - 'A').toByte
-        if (ch >= 'a' && ch <= 'z') return ((ch - 'a') + 26).toByte
-        if (ch >= '0' && ch <= '9') return ((ch - '0') + 52).toByte
-        if (ch == '+') return 62.toByte
-        if (ch == '/') return 63.toByte
-        255.toByte
+        if (ch >= 'A' && ch <= 'Z') (ch - 'A').toByte
+        else if (ch >= 'a' && ch <= 'z') ((ch - 'a') + 26).toByte
+        else if (ch >= '0' && ch <= '9') ((ch - '0') + 52).toByte
+        else if (ch == '+') 62.toByte
+        else if (ch == '/') 63.toByte
+        else 255.toByte
     }
 }
