2c0879b118d74e205031dad2f477d8447590409f
Oleksii Koshterek <oleksii.koshterek@irdeto.com>
2017-04-12 08:45:53 +0000

    Added simple neural network

---
+++ multi-gammon-core/src/main/java/org/akoshterek/backgammon/nn/Unit.java
@@ -0,0 +1,21 @@
+package org.akoshterek.backgammon.nn;
+
+import java.io.Serializable;
+
+
+public interface Unit extends Serializable {
+  
+  /**
+   * Recomputes the value of this hidden unit, querying it's
+   * prior inputs.
+   */
+  public void recompute();
+  
+  /**
+   * Returns the current value of this input
+   *
+   * @return The current value of this input
+   */
+  public double getValue();
+  
+}
