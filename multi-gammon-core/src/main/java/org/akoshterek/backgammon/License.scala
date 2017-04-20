55886812e65615ea02dc3d17ab52f7a0f011524f
Oleksii Koshterek <oleksii.koshterek@irdeto.com>
2017-04-20 14:13:15 +0000

    refactor

---
+++ multi-gammon-core/src/main/java/org/akoshterek/backgammon/License.scala
@@ -0,0 +1,33 @@
+package org.akoshterek.backgammon
+
+import org.apache.commons.io.IOUtils
+
+import resource.managed
+
+/**
+ * @author Alex
+ *         date 04.08.2015.
+ */
+object License {
+    def printLicense(): Unit = {
+        printResourse("/org/akoshterek/backgammon/COPYING")
+    }
+
+  def printWarranty(): Unit = {
+        printResourse("/org/akoshterek/backgammon/WARRANTY")
+    }
+
+  def printBanner(): Unit = {
+        printResourse("/org/akoshterek/backgammon/BANNER")
+    }
+
+    private def printResourse(resourceName: String) {
+      managed(classOf[License].getResourceAsStream(resourceName)).acquireAndGet(inputStream => {
+        IOUtils.copy(inputStream, System.out)
+      })
+     }
+}
+
+class License {
+
+}
