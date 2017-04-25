871d068979764c6dc0877230b521a84391ff9c9d
Oleksii Koshterek <oleksii.koshterek@irdeto.com>
2017-04-25 11:50:46 +0000

    refactor

--- multi-gammon-core/src/main/java/org/akoshterek/backgammon/util/OptionsBean.scala
+++ multi-gammon-core/src/main/java/org/akoshterek/backgammon/util/OptionsBean.scala
@@ -4,10 +4,18 @@ class OptionsBean {
   var isHelp: Boolean = false
   var isWarranty: Boolean = false
   var isLicense: Boolean = false
-  var agentNames: Array[String] = _
-  var benchmarkAgentName: String = _
+  private var _agentNames: Vector[String] = Vector()
+  var benchmarkAgentName: String = ""
   var trainingGames: Int = 0
   var benchmarkGames: Int = 0
   var benchmarkPeriod: Int = 0
   var isVerbose: Boolean = false
+
+  def agentNames = _agentNames
+  def agentNames_= (agents: Array[String]): Unit = {
+    agents match {
+      case null => _agentNames = Vector()
+      case _ => _agentNames = agents.toVector
+    }
+  }
 }
