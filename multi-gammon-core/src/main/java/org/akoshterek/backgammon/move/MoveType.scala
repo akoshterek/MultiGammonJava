8cbdce06a75af53f9aecd39c6f6b25bf32e0e5fe
akoshterek <koshterek@gmail.om>
2017-05-05 21:04:12 +0200

    finished board noreturn

---
+++ multi-gammon-core/src/main/java/org/akoshterek/backgammon/move/MoveType.scala
@@ -0,0 +1,17 @@
+package org.akoshterek.backgammon.move
+
+/**
+  * @author Alex
+  *         date 20.07.2015.
+  */
+sealed trait MoveType {
+    def value: Int
+}
+
+object MoveType {
+    case object MOVE_INVALID extends MoveType {val value: Int = -1}
+    case object MOVE_GAMEINFO extends MoveType {val value: Int = 0}
+    case object MOVE_NORMAL extends MoveType {val value: Int = 1}
+    case object MOVE_SETBOARD extends MoveType {val value: Int = 6}
+    case object MOVE_SETDICE extends MoveType {val value: Int = 7}
+}
