562354f2baca0c15a7c4caa99c4b50bfb1e3c395
akoshterek <koshterek@gmail.om>
2017-05-05 21:25:00 +0200

    refactor bearoffgammon

--- multi-gammon-core/src/main/java/org/akoshterek/backgammon/eval/Evaluator.java
+++ multi-gammon-core/src/main/java/org/akoshterek/backgammon/eval/Evaluator.java
@@ -9,7 +9,6 @@ import org.akoshterek.backgammon.board.PositionId;
 import org.apache.commons.math3.distribution.UniformIntegerDistribution;
 import org.apache.commons.math3.random.RandomGenerator;
 import org.apache.commons.math3.random.Well19937c;
-import scala.collection.immutable.Vector;
 
 import java.nio.file.Path;
 
@@ -370,8 +369,8 @@ public class Evaluator {
             dummy.anBoard()[1 - side][i] = 0;
         }
 
-        Vector<Object> bgp = BearoffGammon.getRaceBGprobs(dummy.anBoard()[1 - side]);
-        if (bgp.nonEmpty()) {
+        long[] bgp = BearoffGammon.getRaceBGprobs(dummy.anBoard()[1 - side]);
+        if (bgp.length > 0) {
             int k = PositionId.positionBearoff(anBoard.anBoard()[side], pbc1.getnPoints(), pbc1.getnChequers());
             short[] aProb = new short[32];
             float p = 0.0f;
@@ -383,7 +382,7 @@ public class Evaluator {
                 scale *= 36;
                 for (int i = 1; i <= j + side; ++i)
                     sum += aProb[i];
-                p += ((float)((Long)(bgp.apply(j)))) / scale * sum;
+                p += ((float)((Long)(bgp[j]))) / scale * sum;
             }
 
             p /= 65535.0;
