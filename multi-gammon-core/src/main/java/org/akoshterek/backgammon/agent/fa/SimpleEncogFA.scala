4c4fb1df84873ccd3eb6ed7d817766ae265b22a5
akoshterek <koshterek@gmail.om>
2017-05-18 17:52:17 +0200

    remove some redundant code

--- multi-gammon-core/src/main/java/org/akoshterek/backgammon/agent/fa/SimpleEncogFA.scala
+++ multi-gammon-core/src/main/java/org/akoshterek/backgammon/agent/fa/SimpleEncogFA.scala
@@ -1,10 +1,9 @@
 package org.akoshterek.backgammon.agent.fa
 
 import java.io.File
+import java.nio.file.Path
 
-import org.akoshterek.backgammon.Constants
 import org.akoshterek.backgammon.eval.Reward
-import org.akoshterek.backgammon.util.Normalizer
 import org.encog.engine.network.activation.ActivationSigmoid
 import org.encog.mathutil.randomize.RangeRandomizer
 import org.encog.ml.data.MLDataSet
@@ -14,51 +13,49 @@ import org.encog.neural.networks.layers.BasicLayer
 import org.encog.neural.networks.training.propagation.Propagation
 import org.encog.neural.networks.training.propagation.back.Backpropagation
 import org.encog.persist.EncogDirectoryPersistence
-import java.nio.file.Path
 
 object SimpleEncogFA {
-    def createNN(inputNeurons: Int, hiddenNeurons: Int): BasicNetwork = {
-        val network: BasicNetwork = new BasicNetwork
-        network.addLayer(new BasicLayer(null, false, inputNeurons))
-        network.addLayer(new BasicLayer(new ActivationSigmoid, false, hiddenNeurons))
-        network.addLayer(new BasicLayer(new ActivationSigmoid, false, 1))
-        network.getStructure.finalizeStructure()
-        new RangeRandomizer(-0.1, 0.1).randomize(network)
-        network.reset()
-        network
-    }
+  def createNN(inputNeurons: Int, hiddenNeurons: Int): BasicNetwork = {
+    val network: BasicNetwork = new BasicNetwork
+    network.addLayer(new BasicLayer(null, false, inputNeurons))
+    network.addLayer(new BasicLayer(new ActivationSigmoid, false, hiddenNeurons))
+    network.addLayer(new BasicLayer(new ActivationSigmoid, false, 1))
+    network.getStructure.finalizeStructure()
+    new RangeRandomizer(-0.1, 0.1).randomize(network)
+    network.reset()
+    network
+  }
 
-    def loadNN(file: File): BasicNetwork = {
-        EncogDirectoryPersistence.loadObject(file).asInstanceOf[BasicNetwork]
-    }
+  def loadNN(file: File): BasicNetwork = {
+    EncogDirectoryPersistence.loadObject(file).asInstanceOf[BasicNetwork]
+  }
 
-    def loadNNFromResource(resource: String): BasicNetwork = {
-        EncogDirectoryPersistence.loadResourceObject(resource).asInstanceOf[BasicNetwork]
-    }
+  def loadNNFromResource(resource: String): BasicNetwork = {
+    EncogDirectoryPersistence.loadResourceObject(resource).asInstanceOf[BasicNetwork]
+  }
 }
 
 class SimpleEncogFA(override val network: BasicNetwork) extends AbsNeuralNetworkFA(network) {
-    private val trainingSet: MLDataSet = new BasicNeuralDataSet(
-        Array[Array[Double]](new Array[Double](network.getInputCount)),
-        Array[Array[Double]](new Array[Double](network.getOutputCount))
-    )
+  private val trainingSet: MLDataSet = new BasicNeuralDataSet(
+    Array[Array[Double]](new Array[Double](network.getInputCount)),
+    Array[Array[Double]](new Array[Double](network.getOutputCount))
+  )
 
-    private val propagation: Propagation = new Backpropagation(network, trainingSet, 0.05, 0)
+  private val propagation: Propagation = new Backpropagation(network, trainingSet, 0.05, 0)
 
-    override def saveNN(file: Path) {
-        EncogDirectoryPersistence.saveObject(file.toFile, network)
-    }
+  override def saveNN(file: Path) {
+    EncogDirectoryPersistence.saveObject(file.toFile, network)
+  }
 
-    override def calculateReward(input: Array[Double]): Reward = {
-        val reward = Reward.rewardArray
-        network.compute(input, reward)
-        new Reward(Normalizer.fromSmallerSigmoid(reward))
-    }
+  override def calculateReward(input: Array[Double]): Reward = {
+    val reward = Reward.rewardArray
+    network.compute(input, reward)
+    new Reward(reward)
+  }
 
-    override def setReward(input: Array[Double], reward: Reward) {
-        val output = Normalizer.toSmallerSigmoid(reward.data.toArray, Constants.NUM_OUTPUTS)
-        trainingSet.get(0).getInput.setData(input)
-        trainingSet.get(0).getIdeal.setData(output)
-        propagation.iteration()
-    }
+  override def setReward(input: Array[Double], reward: Reward) {
+    trainingSet.get(0).getInput.setData(input)
+    trainingSet.get(0).getIdeal.setData(reward.data)
+    propagation.iteration()
+  }
 }
--- multi-gammon-core/src/main/java/org/akoshterek/backgammon/agent/raw/RawBatch40.scala
+++ multi-gammon-core/src/main/java/org/akoshterek/backgammon/agent/raw/RawBatch40.scala
@@ -1,14 +1,12 @@
 package org.akoshterek.backgammon.agent.raw
 
-import org.akoshterek.backgammon.Constants
+import java.nio.file.Path
+
 import org.akoshterek.backgammon.agent.AbsFlexAgent
 import org.akoshterek.backgammon.agent.fa.SimpleEncogFA
-import org.akoshterek.backgammon.agent.inputrepresentation.GnuBgCodec
-import org.akoshterek.backgammon.agent.inputrepresentation.Tesauro89Codec
+import org.akoshterek.backgammon.agent.inputrepresentation.{GnuBgCodec, Tesauro89Codec}
 import org.akoshterek.backgammon.board.Board
 import org.akoshterek.backgammon.eval.Reward
-import org.akoshterek.backgammon.util.Normalizer
-import java.nio.file.Path
 
 class RawBatch40(override val path: Path) extends AbsFlexAgent("RawBatch40", path) {
   contactRepresentation = new RawRepresentation(Tesauro89Codec)
@@ -20,14 +18,14 @@ class RawBatch40(override val path: Path) extends AbsFlexAgent("RawBatch40", pat
   crashedFa = new SimpleEncogFA(SimpleEncogFA.loadNNFromResource("org/akoshterek/backgammon/agent/raw/Raw-GnuBg-40-crashed.eg"))
 
   override def evalContact(board: Board): Reward = {
-    new Reward(Normalizer.fromSmallerSigmoid(super.evalContact(board).data.toArray, Constants.NUM_OUTPUTS))
+    super.evalContact(board)
   }
 
   override def evalRace(board: Board): Reward = {
-    new Reward(Normalizer.fromSmallerSigmoid(super.evalRace(board).data.toArray, Constants.NUM_OUTPUTS))
+    super.evalRace(board)
   }
 
   override def evalCrashed(board: Board): Reward = {
-    new Reward(Normalizer.fromSmallerSigmoid(super.evalCrashed(board).data.toArray, Constants.NUM_OUTPUTS))
+    super.evalCrashed(board)
   }
 }
