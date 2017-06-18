06dbdc231b0f414dce313343a06b48c55aa5b9a6
akoshterek <koshterek@gmail.com>
2017-06-18 17:59:41 +0200

    Codecs to single file

--- multi-gammon-core/src/main/java/org/akoshterek/backgammon/agent/inputrepresentation/PointCodec.scala
+++ multi-gammon-core/src/main/java/org/akoshterek/backgammon/agent/inputrepresentation/PointCodec.scala
@@ -1,6 +1,6 @@
 package org.akoshterek.backgammon.agent.inputrepresentation
 
-trait PointCodec {
+sealed trait PointCodec {
   /**
     * @return amount of inputs per point
     */
@@ -14,4 +14,44 @@ trait PointCodec {
   def point(men: Int, index: Int): Float
 
   val self: this.type = this
+}
+
+object SuttonCodec extends PointCodec {
+  override def point(men: Int, index: Int): Float = index match {
+    case 0 => if (men >= 1) 1.0f else 0.0f
+    case 1 => if (men >= 2) 1.0f else 0.0f
+    case 2 => if (men >= 3) 1.0f else 0.0f
+    case 3 => if (men >= 4) (men - 3) / 12.0f else 0.0f
+    case _ => throw new IllegalArgumentException("Invalid index")
+  }
+}
+
+object Tesauro89Codec extends PointCodec {
+  override def point(men: Int, index: Int): Float = index match {
+    case 0 => if (men == 1) 1.0f else 0.0f
+    case 1 => if (men == 2) 1.0f else 0.0f
+    case 2 => if (men == 3) 1.0f else 0.0f
+    case 3 => if (men >= 4) (men - 3) / 12.0f else 0.0f
+    case _ => throw new IllegalArgumentException("Invalid index")
+  }
+}
+
+object Tesauro92Codec extends PointCodec {
+  override def point(men: Int, index: Int): Float = index match {
+    case 0 => if (men == 1) 1.0f else 0.0f
+    case 1 => if (men >= 2) 1.0f else 0.0f
+    case 2 => if (men == 3) 1.0f else 0.0f
+    case 3 => if (men >= 4) (men - 3) / 12.0f else 0.0f
+    case _ => throw new IllegalArgumentException("Invalid index")
+  }
+}
+
+object GnuBgCodec extends PointCodec {
+  override def point(men: Int, index: Int): Float = index match {
+    case 0 => if (men == 1) 1.0f else 0.0f
+    case 1 => if (men == 2) 1.0f else 0.0f
+    case 2 => if (men >= 3) 1.0f else 0.0f
+    case 3 => if (men >= 4) (men - 3) / 12.0f else 0.0f
+    case _ => throw new IllegalArgumentException("Invalid index")
+  }
 }
